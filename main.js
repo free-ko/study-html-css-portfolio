badbea80ac5f04f9b701015b607fdbc004551de6
kyw <duddnr654@naver.com>
2020-06-09 00:13:58 +0900

    navbar 오류 찾기

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-'use strict';
+"use strict";
 
-
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height; //높이 가져오기
 
-//스크롤이 될때마다 이 함 수를 실행시켜줘
-document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+//스크롤이 될때마다 이 함수를 실행시켜줘
+document.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight: ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
 });
